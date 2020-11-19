ff225b44562c2e59dae3014ece16942cb65d58b4
Stephen Brimhall <stephen.brimhall@gmail.com>
2020-11-19 12:29:15 -0700

    Updated PATH environment to be more stable

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -1,4 +1,10 @@
-export PATH=.:$HOME/.local/bin:$PATH
+export PATH_pyenv=$HOME/.pyenv/shims:/opt/pyenv/pyenv-virtualenv/shims
+export PATH_java=/usr/lib/jvm/default/bin
+export PATH_perl=/usr/bin/vendor_perl:/usr/bin/core_perl
+export PATH_system=/usr/local/sbin:/usr/local/bin:/usr/bin
+export PATH_user=$HOME/.local/bin
+
+export PATH=.:$PATH_pyenv:$PATH_user:$PATH_system:$PATH_java:$PATH_perl
 
 export GPG_TTY=$(tty)
 gpg-connect-agent updatestartuptty /bye &> /dev/null
