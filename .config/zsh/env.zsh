8da5ce487a4762e9b049100f69bb290395215779
Stephen Brimhall <stephen.brimhall@aggiemail.usu.edu>
2021-02-25 21:46:27 +0000

    Completed initial version of Sandbox config

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -7,6 +7,7 @@ export PATH_user=$HOME/.local/bin
 export PATH=.:$PATH_pyenv:$PATH_user:$PATH_system:$PATH_java:$PATH_perl
 
 export GPG_TTY=$(tty)
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 gpg-connect-agent updatestartuptty /bye &> /dev/null
 
 export EDITOR="emacsclient -t"
